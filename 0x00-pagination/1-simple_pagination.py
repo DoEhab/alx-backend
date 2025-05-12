5720347fb4da30e3a3886a6625ce521add413b30
dohaehab <dohaehab@gmail.com>
2025-05-12 20:54:03 +0300

    task 0

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+function get_page
+"""
+from typing import Tuple
+
+import csv
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """
+
+        :param page: page number
+        :param page_size: the number of records
+        :return: tuple
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+
+        :param page: page number
+        :param page_size: page size
+        :return: list of result
+        """
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        start_ind, end_index = self.index_range(page, page_size)
+        result = self.dataset()
+        if start_ind >= len(result):
+            return []
+        return result[start_ind, end_index]
