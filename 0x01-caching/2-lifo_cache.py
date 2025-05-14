8ba5dcfe6407754c36e488bd5afdeb6e6da887b3
dohaehab <dohaehab@gmail.com>
2025-05-14 22:09:59 +0300

    task 0

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,17 +2,43 @@
 """
 LIFOCache module
 """
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ first in first out cache defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
+    """
+    last in first out cache defines:
     """
 
+    def __init__(self):
+
+        super().__init__()
+        self.cache_data = OrderedDict()
+
     def put(self, key, item):
-        pass
+        """
+
+        :param key: dictionary key
+        :param item: dictionary item
+        :return: none
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    oldest_key, _ = self.cache_data.popitem(last=True)
+                    print("DISCARD:", oldest_key)
+                self.cache_data[key] = item
 
     def get(self, key):
-        pass
+        """
+
+        :param key: dictionary key
+        :return: the value
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
