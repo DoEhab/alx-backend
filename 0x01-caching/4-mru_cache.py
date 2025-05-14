e229b2900c4a21122ebb0ed19640c2d19b702ac0
dohaehab <dohaehab@gmail.com>
2025-05-14 23:23:07 +0300

    task 0

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+LIFOCache module
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Least recently used cache
+    """
+
+    def __init__(self):
+
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+
+        :param key: dictionary key
+        :param item: dictionary item
+        :return: none
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+                self.cache_data[key] = item
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    oldest_key, _ = self.cache_data.popitem(last=True)
+                    print("DISCARD:", oldest_key)
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """
+
+        :param key: dictionary key
+        :return: the value
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
