f755073ed293a5e960849becd0af4ec87b7c3a2a
dohaehab <dohaehab@gmail.com>
2025-05-21 05:50:57 +0300

    task 1

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+flask app
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """
+    lang configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_user():
+    """
+    Retrieve user dictionary based on 'login_as' URL parameter
+    """
+    try:
+        user_id = int(request.args.get('login_as'))
+        return users.get(user_id)
+    except (TypeError, ValueError):
+        return None
+
+
+@app.before_request
+def before_request():
+    """
+    Executed before each request
+    """
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale():
+    """
+    determine the best match with our supported languages
+    """
+    locale_param = request.args.get("locale")
+    if locale_param in Config.LANGUAGES:
+        return locale_param
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def basic_app():
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
