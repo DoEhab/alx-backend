79a74fd63b74f3a77a7edad6f7bdeb1ce1ec2919
dohaehab <dohaehab@gmail.com>
2025-05-23 22:11:48 +0300

    task 1

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -33,7 +33,7 @@ def basic_app():
     """
     :return: html page
     """
-    return render_template("3-index.html")
+    return render_template("3-index.html", get_locale=get_locale)
 
 
 if __name__ == '__main__':
